f87fb4331fc0800149fa125d2b4d6c2fb6cfe753
jonas18121 <jonasxtrm971@gmail.com>
2021-01-11 19:36:03 +0100

    projet symfony formulaire

--- php/listing_bulletin_symfony/src/Controller/EleveController.php
+++ php/listing_bulletin_symfony/src/Controller/EleveController.php
@@ -4,14 +4,21 @@ namespace App\Controller;
 
 use App\Entity\Eleve;
 use App\Repository\EleveRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EleveController extends AbstractController
 {
     /**
-     * @Route("/eleve", name="eleve")
+     * @Route("/", name="eleve", methods="GET")
      */
     public function index(EleveRepository $repo): Response
     {
@@ -23,16 +30,53 @@ class EleveController extends AbstractController
     }
 
     /**
-     * @Route("/eleve/{id}", name="eleve_show")
+     * @Route("/eleve/{id}", name="eleve_show", requirements={"id": "\d+"}, methods="GET")
      */
-    public function get_one_eleve_and_edit($id)
+    public function get_one_eleve_and_edit(Eleve $eleve)
     {
-        $repo =  $this->getDoctrine()->getRepository(Eleve::class);
-
-        $eleve = $repo->find($id);
-
-        return $this->render('eleve/show.html.twig', [
+        return $this->render('eleve/get_one_eleve.html.twig', [
             'eleve' => $eleve
+        ]);
+    }
+
+    /**
+     * @Route("/eleve/add", name="eleve_add", methods={"GET", "POST"})
+     */
+    public function create_eleve(Request $request, EntityManagerInterface $manager)
+    {
+        $eleve = new Eleve();
+
+        $form = $this->createFormBuilder($eleve)
+                    ->add('nom', TextType::class, [
+                        'attr' => [
+                            'placeholder' => 'Le nom de l\'élève'
+                        ]
+                    ])
+                    ->add('prenom', TextType::class, [
+                        'attr' => [
+                            'placeholder' => 'Le prénom de l\'élève'
+                        ]
+                    ])
+                    ->add('dateNaissanceAt', DateType::class, [
+                        'attr' => [
+                            'placeholder' => 'La date de naissance de l\'élève'
+                        ]
+                    ])
+                    ->add('moyenne', IntegerType::class, [
+                        'attr' => [
+                            'placeholder' => 'La moyenne de l\'élève'
+                        ]
+                    ])
+                    ->add('appreciation', TextareaType::class, [
+                        'attr' => [
+                            'placeholder' => 'L\'appréciation de l\'élève'
+                        ]
+                    ])
+                    ->getForm()
+        ;
+
+        return $this->render('eleve/create_eleve.html.twig', [
+            'formEleve' => $form->createView()
         ]);
     }
 }
